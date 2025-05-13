2459186887f9bf805b593686251466cf1cc49573
anjiattri <attri2707@gmail.com>
2025-05-13 23:57:09 +0530

    sse

--- Communication/serverSideEvents/index.mjs
+++ Communication/serverSideEvents/index.mjs
@@ -8,7 +8,20 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-app.get("/sse", (req, res) => {});
+app.get("/sse", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Cache-Control", "no-cache");
+  res.write("data: welcome to server sent event\n\n");
+
+  const interval = setInterval(() => {
+    res.write(`server time: ${Date.now()}\n\n`);
+  }, 5000);
+
+  req.on("close", () => {
+    clearInterval(interval);
+  });
+});
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
