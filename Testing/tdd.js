62f0f1a8cda10a9710c14eca33c338dfd7cadd7f
anjiattri <attri2707@gmail.com>
2025-05-29 13:37:03 +0530

    tddd refactor

--- Testing/tdd.js
+++ Testing/tdd.js
@@ -1,10 +1,8 @@
 const isPalindrome = (x) => {
-  if (!x) return null;
-  if (x.length > 10) return null;
   if (typeof x === "number") {
     x = Math.abs(x).toString();
   }
-  if (typeof x !== "string") return null;
+  if (!x || x.length > 10 || typeof x !== "string") return null;
 
   const reverse = x.split("").reverse().join("");
   return reverse.toLowerCase().trim() === x.toLowerCase().trim();
